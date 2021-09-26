9d9c58ff7122478f0e611c459eb96c2b49eead22
andy <deifmo@yandex.ru>
2021-09-26 18:25:57 +0300

    Permissions tests v2

--- users_app/tests/test_permissions.py
+++ users_app/tests/test_permissions.py
@@ -1,3 +1,16 @@
+"""
+This module contains tests for listed below permission classes:
+    CustomIsAuthenticated,
+    CustomIsAdminUser,
+    IsReadOnly,
+    IsWriteOnly,
+    IsActive,
+    IsOwner.
+It tests if they applied and execute correctly in:
+    UserCreateList view GET POST methods. (in tests titles UserCreateList shortcutted to ucl)
+    UserDetail view GET PUT PATCH DELETE methods. (in tests titles UserDetail shortcutted to ud)
+"""
+
 import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
@@ -62,8 +75,13 @@ def admin_token(admin):
 
 
 @pytest.fixture
-def api_client():
-    return APIClient()
+def active_user1_detail_url(active_user1):
+    return reverse('user_detail', kwargs={'pk': active_user1.pk})
+
+
+@pytest.fixture
+def inactive_user_detail_url(inactive_user):
+    return reverse('user_detail', kwargs={'pk': inactive_user.pk})
 
 
 @pytest.fixture
@@ -71,23 +89,37 @@ def user_list_create_url():
     return reverse('user_list_create')
 
 
-# UserListCreate view GET method tests.
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+# UserListCreate view GET method tests
 
 @pytest.mark.django_db
-def test_user_list_create_get_method_is_authenticated_permission(api_client, user_list_create_url):
+def test_ulc_get_method_is_not_allowed_to_unauthenticated_user(api_client, user_list_create_url):
+    """
+    Tests CustomIsAuthenticated permission applied.
+    """
     response = api_client.get(user_list_create_url)
     assert response.status_code == 401
 
 
 @pytest.mark.django_db
-def test_user_list_create_get_method_is_active_permission(api_client, user_list_create_url, inactive_user_token):
+def test_ulc_get_method_is_not_allowed_to_inactive_user(api_client, user_list_create_url, inactive_user_token):
+    """
+    Tests IsActive permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + inactive_user_token.key)
     response = api_client.get(user_list_create_url)
     assert response.status_code == 401
 
 
 @pytest.mark.django_db
-def test_user_list_create_get_method_allowed_to_active_authenticated_user(api_client, user_list_create_url, active_user1_token):
+def test_ulc_get_method_is_allowed_to_active_authenticated_user(api_client, user_list_create_url, active_user1_token):
+    """
+    Tests both CustomIsAuthenticated and IsActive permissions received.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + active_user1_token.key)
     response = api_client.get(user_list_create_url)
     assert response.status_code == 200
@@ -96,7 +128,10 @@ def test_user_list_create_get_method_allowed_to_active_authenticated_user(api_cl
 # UserListCreate view POST method tests.
 
 @pytest.mark.django_db
-def test_user_list_create_post_method_allowed_to_any_user(api_client, user_list_create_url):
+def test_ulc_post_method_is_allowed_to_any_user(api_client, user_list_create_url):
+    """
+    Tests IsWriteOnly permission applied.
+    """
     response = api_client.post(user_list_create_url, data=ACTIVE_USER1_DATA)
     assert response.status_code == 201
 
@@ -104,146 +139,182 @@ def test_user_list_create_post_method_allowed_to_any_user(api_client, user_list_
 # UserDetail view GET method tests.
 
 @pytest.mark.django_db
-def test_user_detail_get_method_is_authenticated_permission(api_client, active_user1):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
-    response = api_client.get(url)
+def test_ud_get_method_is_not_allowed_to_unauthenticated_user(api_client, active_user1_detail_url):
+    """
+    Tests CustomIsAuthenticated permission applied.
+    """
+    response = api_client.get(active_user1_detail_url)
     assert response.status_code == 401
 
 
 @pytest.mark.django_db
-def test_user_detail_get_method_is_active_permission(api_client, active_user1, inactive_user_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_get_method_is_not_allowed_to_inactive_user(api_client, active_user1_detail_url, inactive_user_token):
+    """
+    Tests IsActive permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + inactive_user_token.key)
-    response = api_client.get(url)
+    response = api_client.get(active_user1_detail_url)
     assert response.status_code == 401
 
 
 @pytest.mark.django_db
-def test_user_detail_get_method_allowed_to_active_authenticated_user(api_client, active_user1, active_user2_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_get_method_is_allowed_to_active_authenticated_user(api_client, active_user1_detail_url, active_user2_token):
+    """
+    Tests CustomIsAuthenticated, IsActive and IsReadOnly permissions received.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + active_user2_token.key)
-    response = api_client.get(url)
+    response = api_client.get(active_user1_detail_url)
     assert response.status_code == 200
 
 
 # UserDetail view PUT method tests.
 
 @pytest.mark.django_db
-def test_user_detail_put_method_is_authenticated_permission(api_client, active_user1):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
-    response = api_client.put(url, data=PUT_DATA)
+def test_ud_put_method_is_not_allowed_to_unauthenticated_user(api_client, active_user1_detail_url):
+    """
+    Tests CustomIsAuthenticated permission applied.
+    """
+    response = api_client.put(active_user1_detail_url, data=PUT_DATA)
     assert response.status_code == 401
 
 
 @pytest.mark.django_db
-def test_user_detail_put_method_is_active_permission(api_client, inactive_user, inactive_user_token):
-    url = reverse('user_detail', kwargs={'pk': inactive_user.pk})
+def test_ud_put_method_is_not_allowed_to_inactive_user(api_client, inactive_user_detail_url, inactive_user_token):
+    """
+    Tests IsActive permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + inactive_user_token.key)
-    response = api_client.put(url, data=PUT_DATA)
+    response = api_client.put(inactive_user_detail_url, data=PUT_DATA)
     assert response.status_code == 401
 
 
 @pytest.mark.django_db
-def test_user_detail_put_method_is_owner_permission(api_client, active_user1, active_user2_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_put_method_is_not_allowed_to_not_owner(api_client, active_user1_detail_url, active_user2_token):
+    """
+    Tests IsOwner permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + active_user2_token.key)
-    response = api_client.put(url, data=PUT_DATA)
+    response = api_client.put(active_user1_detail_url, data=PUT_DATA)
     assert response.status_code == 403
 
 
 @pytest.mark.django_db
-def test_user_detail_put_method_allowed_to_active_authenticated_owner(api_client, active_user1, active_user1_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_put_method_is_allowed_to_active_authenticated_owner(api_client, active_user1_detail_url, active_user1_token):
+    """
+    Tests CustomIsAuthenticated, IsActive and IsOwner permissions received.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + active_user1_token.key)
-    response = api_client.put(url, data=PUT_DATA)
+    response = api_client.put(active_user1_detail_url, data=PUT_DATA)
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_user_detail_put_method_allowed_to_admin(api_client, active_user1, admin_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_put_method_is_allowed_to_admin(api_client, active_user1_detail_url, admin_token):
+    """
+    Tests CustomIsAdmin permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + admin_token.key)
-    response = api_client.put(url, data=PUT_DATA)
+    response = api_client.put(active_user1_detail_url, data=PUT_DATA)
     assert response.status_code == 200
 
 
 # UserDetail view PATCH method tests.
 
 @pytest.mark.django_db
-def test_user_detail_patch_method_is_authenticated_permission(api_client, active_user1):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
-    response = api_client.patch(url, data=PATCH_DATA)
+def test_ud_patch_method_is_not_allowed_to_unauthenticated_user(api_client, active_user1_detail_url):
+    """
+    Tests CustomIsAuthenticated permission applied.
+    """
+    response = api_client.patch(active_user1_detail_url, data=PATCH_DATA)
     assert response.status_code == 401
 
 
 @pytest.mark.django_db
-def test_user_detail_patch_method_is_active_permission(api_client, inactive_user, inactive_user_token):
-    url = reverse('user_detail', kwargs={'pk': inactive_user.pk})
+def test_ud_patch_method_is_not_allowed_to_inactive_user(api_client, inactive_user_detail_url, inactive_user_token):
+    """
+    Tests IsActive permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + inactive_user_token.key)
-    response = api_client.patch(url, data=PATCH_DATA)
+    response = api_client.patch(inactive_user_detail_url, data=PATCH_DATA)
     assert response.status_code == 401
 
 
 @pytest.mark.django_db
-def test_user_detail_patch_method_is_owner_permission(api_client, active_user1, active_user2_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_patch_method_is_not_allowed_to_not_owner(api_client, active_user1_detail_url, active_user2_token):
+    """
+    Tests IsOwner permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + active_user2_token.key)
-    response = api_client.patch(url, data=PATCH_DATA)
+    response = api_client.patch(active_user1_detail_url, data=PATCH_DATA)
     assert response.status_code == 403
 
 
 @pytest.mark.django_db
-def test_user_detail_patch_method_allowed_to_active_authenticated_owner(api_client, active_user1, active_user1_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_patch_method_is_allowed_to_active_authenticated_owner(api_client, active_user1_detail_url, active_user1_token):
+    """
+    Tests CustomIsAuthenticated, IsActive and IsOwner permissions received.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + active_user1_token.key)
-    response = api_client.patch(url, data=PATCH_DATA)
+    response = api_client.patch(active_user1_detail_url, data=PATCH_DATA)
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_user_detail_patch_method_allowed_to_admin(api_client, active_user1, admin_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_patch_method_is_allowed_to_admin(api_client, active_user1_detail_url, admin_token):
+    """
+    Tests CustomIsAdmin permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + admin_token.key)
-    response = api_client.patch(url, data=PATCH_DATA)
+    response = api_client.patch(active_user1_detail_url, data=PATCH_DATA)
     assert response.status_code == 200
 
 
 # UserDetail view DELETE method tests.
 
 @pytest.mark.django_db
-def test_user_detail_delete_method_is_authenticated_permission(api_client, active_user1):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
-    response = api_client.delete(url)
+def test_ud_delete_method_is_not_allowed_to_unauthenticated_user(api_client, active_user1_detail_url):
+    """
+    Tests CustomIsAuthenticated permission applied.
+    """
+    response = api_client.delete(active_user1_detail_url)
     assert response.status_code == 401
 
 
 @pytest.mark.django_db
-def test_user_detail_delete_method_is_active_permission(api_client, inactive_user, inactive_user_token):
-    url = reverse('user_detail', kwargs={'pk': inactive_user.pk})
+def test_ud_delete_method_is_not_allowed_to_inactive_user(api_client, inactive_user_detail_url, inactive_user_token):
+    """
+    Tests IsActive permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + inactive_user_token.key)
-    response = api_client.delete(url)
+    response = api_client.delete(inactive_user_detail_url)
     assert response.status_code == 401
 
 
 @pytest.mark.django_db
-def test_user_detail_delete_method_is_owner_permission(api_client, active_user1, active_user2_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_delete_method_is_not_allowed_to_not_owner(api_client, active_user1_detail_url, active_user2_token):
+    """
+    Tests IsOwner permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + active_user2_token.key)
-    response = api_client.delete(url)
+    response = api_client.delete(active_user1_detail_url)
     assert response.status_code == 403
 
 
 @pytest.mark.django_db
-def test_user_detail_delete_method_allowed_to_active_authenticated_owner(api_client, active_user1, active_user1_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_delete_method_is_allowed_to_active_authenticated_owner(api_client, active_user1_detail_url, active_user1_token):
+    """
+    Tests CustomIsAuthenticated, IsActive and IsOwner permissions received.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + active_user1_token.key)
-    response = api_client.delete(url)
+    response = api_client.delete(active_user1_detail_url)
     assert response.status_code == 204
 
 
 @pytest.mark.django_db
-def test_user_detail_delete_method_allowed_to_admin(api_client, active_user1, admin_token):
-    url = reverse('user_detail', kwargs={'pk': active_user1.pk})
+def test_ud_delete_method_is_allowed_to_admin(api_client, active_user1_detail_url, admin_token):
+    """
+    Tests CustomIsAdmin permission applied.
+    """
     api_client.credentials(HTTP_AUTHORIZATION='Token ' + admin_token.key)
-    response = api_client.delete(url)
+    response = api_client.delete(active_user1_detail_url)
     assert response.status_code == 204
