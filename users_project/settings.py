479318e69df28f88200e9dd93c0badbbb62736e6
andy <deifmo@yandex.ru>
2021-10-11 01:37:01 +0300

    File storage is defined as environment variable

--- users_project/settings.py
+++ users_project/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = config('FILE_STORAGE', default='django.core.files.storage.FileSystemStorage')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
